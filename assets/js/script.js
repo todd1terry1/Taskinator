44a8e90d72657fb0a2fe0f71b99243311c7bef59
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2021-10-19 00:35:58 -0500

    aloowed to moved from diff select elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
