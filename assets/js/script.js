3d1623712f3976e66d6db0fef1184509c7a6d354
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2021-10-18 23:04:57 -0500

    added edit buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var taskIdCounter = 0;
-
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
